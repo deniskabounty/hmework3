8a2334a78da1d36cb0222d33c374a4e62ac1b97e
kitaxe <d.denas.avd@gmail.com>
2025-10-13 16:17:24 +0300

    Initial commit: Django blog project

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Post
+from .models import Comment
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('slug', 'published_date',)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
